2725e89da3d38ab6b94b3a872252acb0ec635df8
Viettq <taquocviet262@gmail.com>
2024-03-27 22:25:25 +0700

    update : upload img in messager

--- src/views/utilities/ChatApp/input.jsx
+++ src/views/utilities/ChatApp/input.jsx
@@ -6,77 +6,152 @@ import { v4 as uuid } from 'uuid';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { firestore, storage } from '../../../firebase';
 import Img from '../../../assets/images/chat/img.png';
-import { Button, IconButton, Box, TextField } from '@mui/material';
+import { Button, IconButton, Box, Stack, ImageList, ImageListItem, TextField } from '@mui/material';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { styled, useTheme } from '@mui/material/styles';
+
+const StyledProductImg = styled('img')({
+    top: 0,
+    width: 80,
+    height: 80,
+    objectFit: 'cover',
+    position: 'absolute'
+});
 const Input = () => {
     const [text, setText] = useState('');
-    const [img, setImg] = useState(null);
+    const [img, setImg] = useState([]);
 
     const { currentUser } = useContext(AuthContext);
     const { data } = useContext(ChatContext);
 
-    const handleSend = async () => {
-        if (img) {
-            const storageRef = ref(storage, uuid());
+    const handleChangeImages = (e) => {
+        const newImages = Array.from(e.target.files);
+        setImg(newImages);
+    };
+    const handleRemoveImage = (index) => {
+        const updatedImages = [...img];
+        updatedImages.splice(index, 1);
+        setImg(updatedImages);
+    };
 
-            const uploadTask = uploadBytesResumable(storageRef, img);
-
-            uploadTask.on(
-                (error) => {
-                    //TODO:Handle Error
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-                        await updateDoc(doc(firestore, 'chats', data.chatId), {
-                            messages: arrayUnion({
-                                id: uuid(),
-                                text,
-                                senderId: currentUser.uid,
-                                date: Timestamp.now(),
-                                img: downloadURL
-                            })
-                        });
-                    });
-                }
-            );
-        } else {
-            await updateDoc(doc(firestore, 'chats', data.chatId), {
-                messages: arrayUnion({
-                    id: uuid(),
-                    text,
-                    senderId: currentUser.uid,
-                    date: Timestamp.now()
+    const uploadMultipleImages = async (images) => {
+        const promises = [];
+        for (const image of images) {
+            const storageRef = ref(storage, `chat/${currentUser.uid}-${uuid()}`);
+            const uploadTask = uploadBytesResumable(storageRef, image);
+            promises.push(
+                new Promise((resolve, reject) => {
+                    uploadTask.on(
+                        (error) => {
+                            reject(error);
+                        },
+                        () => {
+                            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                                resolve(downloadURL);
+                            });
+                        }
+                    );
                 })
-            });
+            );
         }
 
-        await updateDoc(doc(firestore, 'userChats', currentUser.uid), {
-            [data.chatId + '.lastMessage']: {
-                text
-            },
-            [data.chatId + '.date']: serverTimestamp()
-        });
-
-        await updateDoc(doc(firestore, 'userChats', data.user.id), {
-            [data.chatId + '.lastMessage']: {
-                text
-            },
-            [data.chatId + '.date']: serverTimestamp()
-        });
-
-        setText('');
-        setImg(null);
+        return Promise.all(promises);
     };
+
+    const handleSend = async () => {
+        if (data.chatId) {
+            if (text || img) {
+                try {
+                    const downloadURLs = await uploadMultipleImages(img);
+                    await updateDoc(doc(firestore, 'chats', data.chatId), {
+                        messages: arrayUnion({
+                            id: uuid(),
+                            text,
+                            senderId: currentUser.uid,
+                            date: Timestamp.now(),
+                            img: downloadURLs
+                        })
+                    });
+                    await updateDoc(doc(firestore, 'userChats', currentUser.uid), {
+                        [data.chatId + '.lastMessage']: {
+                            text,
+                            img: downloadURLs || null
+                        },
+                        [data.chatId + '.date']: serverTimestamp()
+                    });
+
+                    await updateDoc(doc(firestore, 'userChats', data.user.id), {
+                        [data.chatId + '.lastMessage']: {
+                            text,
+                            img: downloadURLs || null
+                        },
+                        [data.chatId + '.date']: serverTimestamp()
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+    };
+
     return (
-        <Box className="input">
-            <TextField fullWidth type="text" placeholder="Type something..." onChange={(e) => setText(e.target.value)} value={text} />
-            <Box className="send">
-                <input type="file" style={{ display: 'none' }} id="file" onChange={(e) => setImg(e.target.files[0])} />
-                <IconButton htmlFor="file" component="label">
-                    <AddPhotoAlternateIcon />
-                </IconButton>
-                <Button onClick={handleSend}>Send</Button>
-            </Box>
+        <Box className="input" sx={{ display: 'flex' }}>
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                sx={{
+                    width: '100%',
+                    height: 'auto'
+                }}
+            >
+                {img.length > 0 && (
+                    <Box
+                        sx={{
+                            width: 'auto',
+                            overflowX: 'auto',
+                            maxWidth: '700px'
+                        }}
+                    >
+                        <ImageList cols={100} rowHeight={100} sx={{ overflowY: 'auto' }}>
+                            {img.map((image, index) => (
+                                <Box key={index} sx={{ position: 'relative', width: '100px', height: '100px', margin: '0' }}>
+                                    <ImageListItem sx={{ width: '100%', height: '100%' }}>
+                                        <img
+                                            src={URL.createObjectURL(image)}
+                                            alt={`product-img-${index}`}
+                                            style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                                        />
+                                    </ImageListItem>
+                                    <IconButton sx={{ position: 'absolute', top: 0, right: 1 }} onClick={() => handleRemoveImage(index)}>
+                                        <HighlightOffIcon />
+                                    </IconButton>
+                                </Box>
+                            ))}
+                        </ImageList>
+                    </Box>
+                )}
+                <Box sx={{ width: '100%', display: ' flex' }}>
+                    <TextField
+                        fullWidth
+                        type="text"
+                        placeholder="Type something..."
+                        onChange={(e) => setText(e.target.value)}
+                        value={text}
+                    />
+                    <Box className="send" sx={{ display: 'flex', alignItems: 'center' }}>
+                        <input multiple accept="image/*" type="file" style={{ display: 'none' }} id="file" onChange={handleChangeImages} />
+                        <label htmlFor="file">
+                            <IconButton component="span">
+                                <AddPhotoAlternateIcon />
+                            </IconButton>
+                        </label>
+                        <Button onClick={handleSend}>Send</Button>
+                    </Box>
+                </Box>
+            </Stack>
         </Box>
     );
 };
--- src/views/utilities/ChatApp/message.jsx
+++ src/views/utilities/ChatApp/message.jsx
@@ -2,13 +2,48 @@ import React, { useState, useContext, useEffect, useRef } from 'react';
 import { ChatContext } from 'context/ChatContext';
 import { AuthContext } from 'context/AuthContext';
 import { collection, query, where, getDocs, setDoc, doc, updateDoc, serverTimestamp, getDoc } from 'firebase/firestore';
-import { Container, Stack, Typography, Divider, Chip, Avatar } from '@mui/material';
+import { Container, Stack, Typography, Box, Chip, Avatar, Modal, IconButton } from '@mui/material';
 import { firestore } from '../../../firebase';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Message = ({ message }) => {
     const { currentUser } = useContext(AuthContext);
     const { data } = useContext(ChatContext);
     const [dataForm, setDataForm] = useState([]);
+    const [fullScreenImageIndex, setFullScreenImageIndex] = useState(null);
+    const [images, setImages] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        // Set images array when fullScreenImage changes
+        if (fullScreenImageIndex !== null) {
+            const imgs = message.img.map((image, index) => ({ src: image, index }));
+            setImages(imgs);
+            setCurrentIndex(imgs.findIndex((img) => img.index === fullScreenImageIndex));
+        }
+    }, [fullScreenImageIndex]);
+
+    const handleImageClick = (image, index) => {
+        setFullScreenImageIndex(index);
+    };
+
+    const handleCloseFullScreenImage = () => {
+        setFullScreenImageIndex(null);
+    };
+    const handleKeyDown = (e) => {
+        if (images.length === 0) return;
+
+        switch (e.key) {
+            case 'ArrowLeft':
+                setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+                break;
+            case 'ArrowRight':
+                setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+                break;
+            default:
+                break;
+        }
+    };
 
     const ref = useRef();
     const findUser = async () => {
@@ -43,18 +78,48 @@ const Message = ({ message }) => {
                 <Typography>{message.timeStamp ? new Date(message.timeStamp.toDate()).toLocaleTimeString() : 'just now'}</Typography>
             </Stack>
             <Stack className="messageContent" direction="row" sx={{ width: '300px' }}>
-                {message.text && <Typography variant="body1">{message.text}</Typography>}
-                {message.img && <img src={message.img} alt="" style={{ width: '100%', borderRadius: 4 }} />}
-                {message.img && (
-                    <Chip
-                        label="Image"
-                        variant="outlined"
-                        size="small"
-                        color="primary"
-                        sx={{ position: 'absolute', bottom: 10, right: 10 }}
-                    />
+                {message.text && (
+                    <Typography variant="body1" sx={{ wordWrap: 'break-word' }}>
+                        {message.text}
+                    </Typography>
                 )}
+                <Box>
+                    {message.img &&
+                        message.img.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image}
+                                alt=""
+                                style={{
+                                    width: '90px',
+                                    height: '90px',
+                                    objectFit: 'cover',
+                                    margin: '4px 0'
+                                }}
+                                onClick={() => handleImageClick(image, index)} // Add onClick event
+                            />
+                        ))}{' '}
+                    {message.img && (
+                        <Chip
+                            label="Image"
+                            variant="outlined"
+                            size="small"
+                            color="primary"
+                            sx={{ position: 'absolute', bottom: 10, right: 10 }}
+                        />
+                    )}
+                </Box>
             </Stack>
+            <Modal open={fullScreenImageIndex !== null} onClose={handleCloseFullScreenImage} onKeyDown={handleKeyDown}>
+                <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+                    <IconButton style={{ position: 'absolute', top: 10, right: 10, color: '#fff' }} onClick={handleCloseFullScreenImage}>
+                        <CloseIcon />
+                    </IconButton>
+                    {images.length > 0 && (
+                        <img src={images[currentIndex].src} alt="" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+                    )}
+                </div>
+            </Modal>
         </Container>
     );
 };
